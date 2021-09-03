1fa3d587b0887221f692f1c198b885c3d12b7634
9753124680 <88178738+9753124680@users.noreply.github.com>
2021-09-03 17:29:58 +0300

    Update index.ts

--- step2-01/demo/src/spread/index.ts
+++ step2-01/demo/src/spread/index.ts
@@ -1,24 +1,24 @@
-// Destructuring
+// Destructurarea
 var [a, b, ...rest] = [1, 2, 3, 4];
 console.log(a, b, rest); // 1 2 [3,4]
 
-// Array assignment
+// Atribuirea matricei
 var list = [1, 2];
 list = [...list, 3, 4];
 console.log(list); // [1,2,3,4]
 
-// Object assignment
+// Atribuirea obiectului
 const point2D = { x: 1, y: 2 };
 const point3D = { ...point2D, z: 3 };
 
-// Concat two objects
+// Concatenarea a două obiecte
 const obj1 = { x: 1 };
 const obj2 = { y: 2 };
 
 const obj3 = { ...obj1, ...obj2 };
 
-// Destructuring object
+// Destructurarea obiectului
 const { x } = obj3;
 
-// adding an export turns this into a "module"
+// adăugând un export o va transforma într-un "module"
 export default {};
