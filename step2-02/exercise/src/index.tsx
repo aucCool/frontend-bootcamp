e9552135d843d532304882f209b670b1f4c1a65c
9753124680 <88178738+9753124680@users.noreply.github.com>
2021-09-05 11:59:49 +0300

    Update index.tsx
    tradus

--- step2-02/exercise/src/index.tsx
+++ step2-02/exercise/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { TodoApp } from './components/TodoApp';
 import { initializeIcons } from '@uifabric/icons';
 
-// Initializes the UI Fabric icons that we can use
-// Choose one from this list: https://developer.microsoft.com/en-us/fabric#/styles/icons
+// Inițializați pictogramele UI Fabric pe care le putem utiliza 
+// Alegeți una din această listă: https://developer.microsoft.com/en-us/fabric#/styles/icons
 initializeIcons();
 
 ReactDOM.render(<TodoApp />, document.getElementById('app'));
